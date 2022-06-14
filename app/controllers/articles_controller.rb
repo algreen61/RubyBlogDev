1aba5b56b1c085d998c6cce9e432cdf44b596f8a
“Drew” <riahtel@gmail.com>
2022-06-14 15:11:14 +0000

    updated app 06142022

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :article_set, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @articles=Article.all
   end
 
   def show
-    @article=Article.find(params[:id])
+    #@article=Article.find(params[:id])
   end
 
   def new
@@ -13,13 +15,15 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article=Article.find(params[:id])
+    #@article=Article.find(params[:id])
   end
 
   def create
+    #render plain: params[:article] shows useful stuff... a hash
     @article = Article.new(article_params)
 
     if @article.save
+      flash[:notice] = "Article created succesfully."
       redirect_to @article
     else
       render 'new'
@@ -27,9 +31,10 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article=Article.find(params[:id])
+    #@article=Article.find(params[:id])
 
       if @article.update(article_params)
+        flash[:notice] = "Article updated succesfully."
         redirect_to @article
       else
         render 'edit'
@@ -37,15 +42,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
-
     redirect_to articles_path
   end
 
 
-  private
+  private #only private to the controller mentioned
+
     def article_params
       params.require(:article).permit(:title,:text)
     end
+
+    def article_set
+      @article=Article.find(params[:id])
+    end
+
 end
